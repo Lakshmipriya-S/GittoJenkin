bba3d708a3fb407e4d442bc6bc2dc5a4836ccf1f
Lakshmipriya-S <s.lakshmipriya63@gmail.com>
2021-11-30 21:51:47 +0530

    second commit

--- JenkinFile.java
+++ JenkinFile.java
@@ -1,5 +1,7 @@
 public class JenkinFile{
 	public static void main(String[] args){
 		System.out.println("hello world from git to jenkin - first line");
+		
+		System.out.println("we are into jenkin - second line");
 	}
 }
---
+++ JenkinFile.java.bak
@@ -0,0 +1,5 @@
+public class JenkinFile{
+	public static void main(String[] args){
+		System.out.println("hello world from git to jenkin - first line");
+	}
+}
